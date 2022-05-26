ce15ec9cb240b1bda132dd19147a38b69c894e17
Eldar Dragomir <oh.eldarkaa@gmail.com>
2022-05-27 00:58:15 +0400

    subdomain visit count

---
+++ strings_and_array/subdomain_visit_count.py
@@ -0,0 +1,18 @@
+from typing import *
+
+# 52 / 52 test cases passed.
+# Status: Accepted
+# Runtime: 64 ms
+# Memory Usage: 13.9 MB
+
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        domain_to_count_map = {}
+        for pair in cpdomains:
+            count, domain = pair.split(' ',2)
+            subdomains = domain.split('.')
+            for idx in range(0, len(subdomains)):
+                subdomain = '.'.join(subdomains[idx:])
+                domain_to_count_map[subdomain] = domain_to_count_map.get(subdomain, 0) + int(count)
+            
+        return [f'{count} {domain}' for domain, count in domain_to_count_map.items()]
--- test/test_strings_and_array.py
+++ test/test_strings_and_array.py
@@ -54,3 +54,34 @@ class Test_UniqueEmailAddresses(unittest.TestCase):
         test2 = ["test.gmail+alex@leetcode.com","test.gmail+david@leetcode.com","anna.e.mail+bob.cathy@leetcode.com"]
         self.assertEqual(self.s.numUniqueEmails(test2), 2)
     
+class Test_SubdomainVisitCount(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        unittest.TestCase.__init__(self, *args, **kwargs)
+        
+        from strings_and_array.subdomain_visit_count import Solution
+        self.s = Solution()
+
+    def test_1(self):
+        input = ["9001 discuss.leetcode.com"]
+        self.assertEqual(self.s.subdomainVisits(input), ["9001 discuss.leetcode.com", "9001 leetcode.com", "9001 com"])
+        
+    def test_2(self):
+        input = ["900 google.mail.com"]
+        self.assertEqual(self.s.subdomainVisits(input), ["900 google.mail.com", "900 mail.com", "900 com"])
+    
+    def test_3(self):
+        input = ["1 a.b.c"]
+        self.assertEqual(self.s.subdomainVisits(input), ["1 a.b.c", "1 b.c", "1 c"])
+    
+    def test_4(self):
+        input = ["2 abc.d.ef"]
+        self.assertEqual(self.s.subdomainVisits(input), ["2 abc.d.ef", "2 d.ef", "2 ef"])
+    
+    def test_5(self):
+        input = ["2 abc.d.ef", "2 ghi.j.k"]
+        self.assertEqual(self.s.subdomainVisits(input), ["2 abc.d.ef", "2 d.ef", "2 ef", "2 ghi.j.k", "2 j.k", "2 k"])
+    
+    def test_6(self):
+        input = ["1 a.b.c", "2 ab.c"]
+        self.assertEqual(self.s.subdomainVisits(input), ['1 a.b.c', '1 b.c', '3 c', '2 ab.c'])
+        
